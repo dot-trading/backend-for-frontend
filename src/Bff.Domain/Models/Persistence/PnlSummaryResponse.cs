fb3d21609f7a26b6c068f53715d02fbae842bd8d
Hamza SAII <hamza.saii@outlook.com>
2026-05-04 22:16:26 +0200

    feat: object results

--- src/Bff.Domain/Models/Persistence/PnlSummaryResponse.cs
+++ src/Bff.Domain/Models/Persistence/PnlSummaryResponse.cs
@@ -1,9 +1,30 @@
+using System.Text.Json.Serialization;
+
 namespace Bff.Domain.Models.Persistence;
 
 public class PnlSummaryResponse
 {
-    public double Daily { get; set; }
-    public double Weekly { get; set; }
-    public double Monthly { get; set; }
-    public double Total { get; set; }
+    [JsonPropertyName("today")]
+    public PnlSummaryItemResponse Today { get; set; } = new();
+    
+    [JsonPropertyName("yesterday")]
+    public PnlSummaryItemResponse Yesterday { get; set; } = new();
+    
+    [JsonPropertyName("thisWeek")]
+    public PnlSummaryItemResponse ThisWeek { get; set; } = new();
+    
+    [JsonPropertyName("thisMonth")]
+    public PnlSummaryItemResponse ThisMonth { get; set; } = new();
+    
+    [JsonPropertyName("thisYear")]
+    public PnlSummaryItemResponse ThisYear { get; set; } = new();
+    
+    [JsonPropertyName("total")]
+    public PnlSummaryItemResponse Total { get; set; } = new();
+}
+
+public class PnlSummaryItemResponse
+{
+    [JsonPropertyName("value")]
+    public double Value { get; set; }
 }
